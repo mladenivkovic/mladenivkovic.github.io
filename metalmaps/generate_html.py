fdba5b050f214be1f3f30e41a9868fa36f33e4d6
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-07-09 21:32:25 +0100

    sorted albums alphabetically

--- metalmaps/generate_html.py
+++ metalmaps/generate_html.py
@@ -31,6 +31,13 @@ apostasy = cmapContainer(
     "https://coverartarchive.org/release/b9ca5e3c-a82c-3afd-ace3-cc4184723c52/2442134676.jpg",
 )
 
+ashes_of_the_wake = cmapContainer(
+    "ashes_of_the_wake",
+    "Ashes of the Wake",
+    "Lamb of God",
+    "https://coverartarchive.org/release/6012b20e-8fdd-4afc-9b0e-a9f22bce81a1/36090160874.jpg",
+)
+
 black_sabbath = cmapContainer(
     "black_sabbath",
     "Black Sabbath",
@@ -59,81 +66,11 @@ cosmos_factory = cmapContainer(
     "https://coverartarchive.org/release/067c3d38-1f5c-4596-baf3-e494a4342d1f/12385725136.jpg",
 )
 
-london_calling = cmapContainer(
-    "london_calling",
-    "London Calling",
-    "The Clash",
-    "https://coverartarchive.org/release/7bec22a0-eb73-4b79-a619-b253d5c2af87/4527734511.jpg",
-)
-
-master_of_puppets = cmapContainer(
-    "master_of_puppets",
-    "Master of Puppets",
-    "Metallica",
-    "https://coverartarchive.org/release/cb100c18-fb29-4828-8d24-2fdbac6d6cff/8600878942.jpg",
-)
-
-meteora = cmapContainer(
-    "meteora",
-    "Meteora",
-    "Linkin Park",
-    "https://coverartarchive.org/release/dd5fd2b3-576e-4354-bc67-68e6f006fd26/3167538307.jpg",
-)
-
-obzen = cmapContainer(
-    "obzen",
-    "obZen",
-    "Meshuggah",
-    "https://coverartarchive.org/release/fa7028f6-d4ac-4b4c-b87f-7c042d1b5c80/10833262918.jpg",
-)
-
-paranoid = cmapContainer(
-    "paranoid",
-    "Paranoid",
-    "Black Sabbath",
-    "https://coverartarchive.org/release/d820f080-845a-4525-8e46-087ce9f8cdda/23423100131.jpg",
-)
-
-ride_the_lightning = cmapContainer(
-    "ride_the_lightning",
-    "Ride the Lightning",
-    "Metallica",
-    "https://coverartarchive.org/release/589ff96d-0be8-3f82-bdd2-299592e51b40/15674886619.jpg",
-)
-
-ziggy_stardust = cmapContainer(
-    "ziggy_stardust",
-    "The Rise and Fall of Ziggy Stardust and the Spiders From Mars",
-    "David Bowie",
-    "https://coverartarchive.org/release/bb7f0f5a-586e-3f90-8665-fdc547aa2a54/1239299887.jpg",
-)
-
-deep_purple_in_rock = cmapContainer(
-    "deep_purple_in_rock",
-    "Deep Purple in Rock",
-    "Deep Purple",
-    "https://coverartarchive.org/release/5156ea9f-eb54-4258-bbf0-e3af9219de1f/31715408459.jpg",
-)
-
-made_in_japan = cmapContainer(
-    "made_in_japan",
-    "Made in Japan",
-    "Deep Purple",
-    "https://coverartarchive.org/release/fb49386f-541a-4928-a606-1f6c9aacaec5/5977807803.jpg",
-)
-
-dethalbum = cmapContainer(
-    "dethalbum",
-    "The Dethalbum",
-    "Dethklok",
-    "https://coverartarchive.org/release/5379197a-54bf-45f5-95fe-f995fd047d50/19745544468.jpg",
-)
-
-la_woman = cmapContainer(
-    "la_woman",
-    "L.A. Woman",
-    "The Doors",
-    "https://coverartarchive.org/release/e68f23df-61e3-4264-bfc3-17ac3a6f856b/5132812481.jpg",
+fear_of_the_dark = cmapContainer(
+    "fear_of_the_dark",
+    "Fear of the Dark",
+    "Iron Maiden",
+    "https://coverartarchive.org/release/ecc2e9db-637c-4f21-955c-6ab3e1123ffc/1282374549.jpg",
 )
 
 from_mars_to_sirius = cmapContainer(
@@ -143,82 +80,18 @@ from_mars_to_sirius = cmapContainer(
     "https://coverartarchive.org/release/6ea45c08-3cfa-461a-aa4d-4cc404fcfa86/1512690278.jpg",
 )
 
-
-powerslave = cmapContainer(
-    "powerslave",
-    "Powerslave",
-    "Iron Maiden",
-    "https://coverartarchive.org/release/4766bf83-3b58-4584-b75a-43917627b790/2595131402.jpg",
+deep_purple_in_rock = cmapContainer(
+    "deep_purple_in_rock",
+    "Deep Purple in Rock",
+    "Deep Purple",
+    "https://coverartarchive.org/release/5156ea9f-eb54-4258-bbf0-e3af9219de1f/31715408459.jpg",
 )
 
-number_of_the_beast = cmapContainer(
-    "number_of_the_beast",
-    "The Number of the Beast",
-    "Iron Maiden",
-    "https://coverartarchive.org/release/5aa782e6-d87a-3ec1-af46-ff68c76cf8fc/7847268318.jpg",
-)
-
-fear_of_the_dark = cmapContainer(
-    "fear_of_the_dark",
-    "Fear of the Dark",
-    "Iron Maiden",
-    "https://coverartarchive.org/release/ecc2e9db-637c-4f21-955c-6ab3e1123ffc/1282374549.jpg",
-)
-
-painkiller = cmapContainer(
-    "painkiller",
-    "Painkiller",
-    "Judas Priest",
-    "https://coverartarchive.org/release/f3228f88-1315-4f7e-9c68-2acf0d2f6e90/7356437148.jpg",
-)
-
-screaming_for_vengeance = cmapContainer(
-    "screaming_for_vengeance",
-    "Screaming for Vengeance",
-    "Judas Priest",
-    "https://coverartarchive.org/release/57e2798e-d7d0-4250-a17b-0b8fb56ade4f/11916137754.jpg",
-)
-
-ashes_of_the_wake = cmapContainer(
-    "ashes_of_the_wake",
-    "Ashes of the Wake",
-    "Lamb of God",
-    "https://coverartarchive.org/release/6012b20e-8fdd-4afc-9b0e-a9f22bce81a1/36090160874.jpg",
-)
-
-the_hunter = cmapContainer(
-    "the_hunter",
-    "The Hunter",
-    "Mastodon",
-    "https://coverartarchive.org/release/e349eaf2-865c-4b47-b719-29d4b5f8e789/3586926524.jpg",
-)
-
-rock_n_roll = cmapContainer(
-    "rock_n_roll",
-    "Rock 'n' Roll",
-    "Mot&oumlrhead",
-    "https://coverartarchive.org/release/b4bdc9c6-a3c0-4e50-a6f5-fe6ec5e66609/14298490837.jpg",
-)
-
-overkill = cmapContainer(
-    "overkill",
-    "Overkill",
-    "Mot&oumlrhead",
-    "https://coverartarchive.org/release/a7b33578-a44a-47e8-bcb8-e1ba8eb848fc/9157237778.jpg",
-)
-
-reign_in_blood = cmapContainer(
-    "reign_in_blood",
-    "Reign in Blood",
-    "Slayer",
-    "https://coverartarchive.org/release/f09a3d0b-5760-41e5-bd62-1e7403b27522/26676279934.jpg",
-)
-
-south_of_heaven = cmapContainer(
-    "south_of_heaven",
-    "South of Heaven",
-    "Slayer",
-    "https://coverartarchive.org/release/407ade2e-03b4-3951-b244-b97de8fb0433/7844026684.jpg",
+dethalbum = cmapContainer(
+    "dethalbum",
+    "The Dethalbum",
+    "Dethklok",
+    "https://coverartarchive.org/release/5379197a-54bf-45f5-95fe-f995fd047d50/19745544468.jpg",
 )
 
 hypnotize = cmapContainer(
@@ -235,12 +108,139 @@ in_utero = cmapContainer(
     "https://coverartarchive.org/release/80189394-8ca0-4a97-9d0e-aad32725de22/32188969568.jpg",
 )
 
+la_woman = cmapContainer(
+    "la_woman",
+    "L.A. Woman",
+    "The Doors",
+    "https://coverartarchive.org/release/e68f23df-61e3-4264-bfc3-17ac3a6f856b/5132812481.jpg",
+)
+
 lenfant_sauvage = cmapContainer(
     "lenfant_sauvage",
     "L'enfant Sauvage",
     "Gojira",
     "https://coverartarchive.org/release/5b7e9d43-8330-4f91-8e78-81dde869e9b5/16931897915.jpg",
 )
+
+london_calling = cmapContainer(
+    "london_calling",
+    "London Calling",
+    "The Clash",
+    "https://coverartarchive.org/release/7bec22a0-eb73-4b79-a619-b253d5c2af87/4527734511.jpg",
+)
+
+made_in_japan = cmapContainer(
+    "made_in_japan",
+    "Made in Japan",
+    "Deep Purple",
+    "https://coverartarchive.org/release/fb49386f-541a-4928-a606-1f6c9aacaec5/5977807803.jpg",
+)
+
+master_of_puppets = cmapContainer(
+    "master_of_puppets",
+    "Master of Puppets",
+    "Metallica",
+    "https://coverartarchive.org/release/cb100c18-fb29-4828-8d24-2fdbac6d6cff/8600878942.jpg",
+)
+
+meteora = cmapContainer(
+    "meteora",
+    "Meteora",
+    "Linkin Park",
+    "https://coverartarchive.org/release/dd5fd2b3-576e-4354-bc67-68e6f006fd26/3167538307.jpg",
+)
+
+number_of_the_beast = cmapContainer(
+    "number_of_the_beast",
+    "The Number of the Beast",
+    "Iron Maiden",
+    "https://coverartarchive.org/release/5aa782e6-d87a-3ec1-af46-ff68c76cf8fc/7847268318.jpg",
+)
+
+obzen = cmapContainer(
+    "obzen",
+    "obZen",
+    "Meshuggah",
+    "https://coverartarchive.org/release/fa7028f6-d4ac-4b4c-b87f-7c042d1b5c80/10833262918.jpg",
+)
+
+overkill = cmapContainer(
+    "overkill",
+    "Overkill",
+    "Mot&oumlrhead",
+    "https://coverartarchive.org/release/a7b33578-a44a-47e8-bcb8-e1ba8eb848fc/9157237778.jpg",
+)
+
+painkiller = cmapContainer(
+    "painkiller",
+    "Painkiller",
+    "Judas Priest",
+    "https://coverartarchive.org/release/f3228f88-1315-4f7e-9c68-2acf0d2f6e90/7356437148.jpg",
+)
+
+paranoid = cmapContainer(
+    "paranoid",
+    "Paranoid",
+    "Black Sabbath",
+    "https://coverartarchive.org/release/d820f080-845a-4525-8e46-087ce9f8cdda/23423100131.jpg",
+)
+
+powerslave = cmapContainer(
+    "powerslave",
+    "Powerslave",
+    "Iron Maiden",
+    "https://coverartarchive.org/release/4766bf83-3b58-4584-b75a-43917627b790/2595131402.jpg",
+)
+
+reign_in_blood = cmapContainer(
+    "reign_in_blood",
+    "Reign in Blood",
+    "Slayer",
+    "https://coverartarchive.org/release/f09a3d0b-5760-41e5-bd62-1e7403b27522/26676279934.jpg",
+)
+
+ride_the_lightning = cmapContainer(
+    "ride_the_lightning",
+    "Ride the Lightning",
+    "Metallica",
+    "https://coverartarchive.org/release/589ff96d-0be8-3f82-bdd2-299592e51b40/15674886619.jpg",
+)
+
+rock_n_roll = cmapContainer(
+    "rock_n_roll",
+    "Rock 'n' Roll",
+    "Mot&oumlrhead",
+    "https://coverartarchive.org/release/b4bdc9c6-a3c0-4e50-a6f5-fe6ec5e66609/14298490837.jpg",
+)
+
+screaming_for_vengeance = cmapContainer(
+    "screaming_for_vengeance",
+    "Screaming for Vengeance",
+    "Judas Priest",
+    "https://coverartarchive.org/release/57e2798e-d7d0-4250-a17b-0b8fb56ade4f/11916137754.jpg",
+)
+
+south_of_heaven = cmapContainer(
+    "south_of_heaven",
+    "South of Heaven",
+    "Slayer",
+    "https://coverartarchive.org/release/407ade2e-03b4-3951-b244-b97de8fb0433/7844026684.jpg",
+)
+
+the_hunter = cmapContainer(
+    "the_hunter",
+    "The Hunter",
+    "Mastodon",
+    "https://coverartarchive.org/release/e349eaf2-865c-4b47-b719-29d4b5f8e789/3586926524.jpg",
+)
+
+ziggy_stardust = cmapContainer(
+    "ziggy_stardust",
+    "The Rise and Fall of Ziggy Stardust and the Spiders From Mars",
+    "David Bowie",
+    "https://coverartarchive.org/release/bb7f0f5a-586e-3f90-8665-fdc547aa2a54/1239299887.jpg",
+)
+
 
 plotAlbum = lenfant_sauvage
 
